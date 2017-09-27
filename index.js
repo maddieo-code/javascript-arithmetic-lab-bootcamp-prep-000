eda543491b0ce6db7c52d2acc436701db79ec2a3
Maddie O'Meara <maddie.omeara@gmail.com>
2017-09-27 19:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-function add (a,b) {
+function add(a,b) {
   return a + b;
 }
 
-function subtract (a,b) {
+function subtract(a,b) {
   return a - b;
 }
 
-function multiply (a,b) {
+function multiply(a,b) {
   return a * b;
 }
 
-function divide (a,b) {
+function divide(a,b) {
   return a / b;
+}
+
+function inc(n) {
+  return n + 1;
+}
+
+function dec(n) {
+  return n - 1;
+}
+
+function makeInt(string) {
+  return parseInt(string);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
